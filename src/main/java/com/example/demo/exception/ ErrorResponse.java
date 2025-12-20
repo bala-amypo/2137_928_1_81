28f85fc06355217cf2c0de6741ace338e76d35d1
bala-amypo <bala@amypo.in>
2025-12-20 15:14:32 +0000

    User commit

--- src/main/java/com/example/demo/exception/ ErrorResponse.java
+++ src/main/java/com/example/demo/exception/ ErrorResponse.java
@@ -10,7 +10,8 @@ public class ErrorResponse {
     private String message;
     private String path;
 
-    public ErrorResponse() {}
+    public ErrorResponse() {
+    }
 
     public ErrorResponse(LocalDateTime timestamp, int status, String error,
                          String message, String path) {
@@ -21,18 +22,43 @@ public class ErrorResponse {
         this.path = path;
     }
 
-    public LocalDateTime getTimestamp() { return timestamp; }
-    public void setTimestamp(LocalDateTime timestamp) { this.timestamp = timestamp; }
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
 
-    public int getStatus() { return status; }
-    public void setStatus(int status) { this.status = status; }
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 
-    public String getError() { return error; }
-    public void setError(String error) { this.error = error; }
+    public int getStatus() {
+        return status;
+    }
 
-    public String getMessage() { return message; }
-    public void setMessage(String message) { this.message = message; }
+    public void setStatus(int status) {
+        this.status = status;
+    }
 
-    public String getPath() { return path; }
-    public void setPath(String path) { this.path = path; }
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
--- src/main/java/com/example/demo/exception/ GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/ GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.example.demo.exception;
 
 import java.time.LocalDateTime;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
@@ -22,6 +24,7 @@ public class GlobalExceptionHandler {
                 ex.getMessage(),
                 request.getRequestURI()
         );
+
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
@@ -37,6 +40,7 @@ public class GlobalExceptionHandler {
                 ex.getMessage(),
                 request.getRequestURI()
         );
+
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
