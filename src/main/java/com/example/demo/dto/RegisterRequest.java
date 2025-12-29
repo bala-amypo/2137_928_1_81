099f8b6d41b784b7cf78a7751e0d4b71f515109b
bala-amypo <bala@amypo.in>
2025-12-29 08:33:52 +0000

    User commit

--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -11,7 +11,7 @@ public class RegisterRequest {
         this.name = name; this.email = email; this.department = department; this.password = password;
     }
 
-    // Getters & Setters
+    
     public String getName() {return name;} public void setName(String name) {this.name = name;}
     public String getEmail() {return email;} public void setEmail(String email) {this.email = email;}
     public String getDepartment() {return department;} public void setDepartment(String department) {this.department = department;}
